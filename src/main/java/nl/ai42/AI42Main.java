39e85c729ebdf8f65dc32ba1d00ed2d607943ae9
Laurens <laatmijnueffe@gmail.com>
2023-05-26 10:52:27 +0200

    fix: removed unused import

--- src/main/java/nl/ai42/AI42Main.java
+++ src/main/java/nl/ai42/AI42Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.image.Image;
-import java.io.File;
 
 public class AI42Main extends Application {
 
