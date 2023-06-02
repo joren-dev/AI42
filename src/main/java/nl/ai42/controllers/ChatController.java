13440fa8da577a29a1821dc44b7bcccd09ad811b
Joren <contactjorenx@gmail.com>
2023-06-02 22:19:11 +0200

    remove: scrollbar visibility

--- src/main/java/nl/ai42/controllers/ChatController.java
+++ src/main/java/nl/ai42/controllers/ChatController.java
@@ -2,11 +2,47 @@ package nl.ai42.controllers;
 
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 public class ChatController {
 
+    @FXML
+    private VBox conversationListContainer;
+    @FXML
+    private Button startConversationButton;
+
+    private int conversationCount = 0;
+
+    @FXML
+    public void startConversation(ActionEvent actionEvent) {
+        // Create a new conversation button
+        Button newConversationButton = new Button("Conversation " + conversationCount);
+        newConversationButton.setOnAction(this::openConversation);
+
+        // Apply the same style as the startConversationButton
+        newConversationButton.setStyle(startConversationButton.getStyle());
+
+        // Add the new conversation button at the bottom
+        conversationListContainer.getChildren().add(newConversationButton);
+
+        conversationCount++;
+    }
+
+    private void openConversation(ActionEvent actionEvent) {
+        // Retrieve the clicked conversation button
+        Button conversationButton = (Button) actionEvent.getSource();
+
+        // Get the conversation name from the button's text
+        String conversationName = conversationButton.getText();
+
+        // Perform actions to open the conversation and start chatting
+        System.out.println("Opening conversation: " + conversationName);
+        // Add your code here to open the conversation and start chatting
+    }
     public void sendButtonAction(ActionEvent actionEvent) {
         // Handle send button action
     }
@@ -19,8 +55,5 @@ public class ChatController {
     }
 
     public void handleSettingsButton(MouseEvent mouseEvent) {
-    }
-
-    public void startConversation(ActionEvent actionEvent) {
     }
 }
