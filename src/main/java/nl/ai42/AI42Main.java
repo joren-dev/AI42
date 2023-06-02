57dc3aad23e747aa3017a4ad0f6c18d8d02d1d20
Joren <contactjorenx@gmail.com>
2023-06-02 18:50:21 +0200

    impl: scene manager into code

--- src/main/java/nl/ai42/AI42Main.java
+++ src/main/java/nl/ai42/AI42Main.java
@@ -1,12 +1,10 @@
 package nl.ai42;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.image.Image;
+import nl.ai42.managers.SceneManager;
 import nl.ai42.utils.Database;
 
 public class AI42Main extends Application {
@@ -14,42 +12,48 @@ public class AI42Main extends Application {
     private double x_offset = 0;
     private double y_offset = 0;
     public static Database database = new Database("AI42.db");
+    private SceneManager scene_manager;
 
     @Override
     public void start(final Stage stage) throws Exception {
-        if (AI42Main.database.getTable("user") == null)
-            AI42Main.database.createTable("user", new String[]{"username", "email", "password", "date_of_birth", "gender"});
-
-        if (AI42Main.database.getTable("chat") == null)
-            AI42Main.database.createTable("chat", new String[]{"username", "chatname"});
-
-        if (AI42Main.database.getTable("chatmsg") == null)
-            AI42Main.database.createTable("chatmsg", new String[]{"username", "chatname", "msg_counter", "msg_content", "is_ai", "sent"});
-
-        Parent root = FXMLLoader.load(getClass().getResource("views/login-and-register.fxml"));
+        initialize_database();
 
         stage.initStyle(StageStyle.UNDECORATED);
 
+        scene_manager = SceneManager.getInstance(stage);
+        scene_manager.loadScene("login-and-register.fxml");
+
         // grabs your root here
-        root.setOnMousePressed(event -> {
+        scene_manager.getCurrent_scene().getRoot().setOnMousePressed(event -> {
             x_offset = event.getSceneX();
             y_offset = event.getSceneY();
         });
 
         // move window around
-        root.setOnMouseDragged(event -> {
+        scene_manager.getCurrent_scene().getRoot().setOnMouseDragged(event -> {
             stage.setX(event.getScreenX() - x_offset);
             stage.setY(event.getScreenY() - y_offset);
         });
 
-        final Scene scene = new Scene(root);
-
         Image icon = new Image(getClass().getResourceAsStream("images/favicon.png"));
         stage.getIcons().add(icon);
 
-        stage.setScene(scene);
+        stage.setScene(scene_manager.getCurrent_scene());
 
         stage.show();
+    }
+
+    private void initialize_database() throws Exception {
+        database = new Database("AI42.db");
+
+        if (database.getTable("user") == null)
+            database.createTable("user", new String[]{"username", "email", "password", "date_of_birth", "gender"});
+
+        if (database.getTable("chat") == null)
+            database.createTable("chat", new String[]{"username", "chatname"});
+
+        if (database.getTable("chatmsg") == null)
+            database.createTable("chatmsg", new String[]{"username", "chatname", "msg_counter", "msg_content", "is_ai", "sent"});
     }
 
     public static void main(String[] args) {
--- src/main/java/nl/ai42/managers/SceneManager.java
+++ src/main/java/nl/ai42/managers/SceneManager.java
@@ -1,6 +1,46 @@
 package nl.ai42.managers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SceneManager {
-    
+    private static SceneManager instance;
+    private Stage stage;
+    private Scene current_scene;
+
+    private SceneManager(Stage stage) {
+        this.stage = stage;
+    }
+
+    public static SceneManager getInstance(Stage stage) {
+        if (instance == null) {
+            instance = new SceneManager(stage);
+        }
+        return instance;
+    }
+
+    public void loadScene(String fxmlFile) {
+        try {
+            String resourcePath = "/nl/ai42/views/" + fxmlFile;
+            Parent root = FXMLLoader.load(getClass().getResource(resourcePath));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            current_scene = scene;
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void switchScene(String fxmlFile) {
+        loadScene(fxmlFile);
+    }
+
+    public Scene getCurrent_scene() {
+        return current_scene;
+    }
 }
