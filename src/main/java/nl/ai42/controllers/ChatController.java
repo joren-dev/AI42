9eef6614d1f8466ee3fc18a383bbc57bf8bcaaaf
Joren <contactjorenx@gmail.com>
2023-06-02 21:48:33 +0200

    fix: many redundant files, styling chat-view

--- src/main/java/nl/ai42/controllers/ChatController.java
+++ src/main/java/nl/ai42/controllers/ChatController.java
@@ -3,12 +3,24 @@ package nl.ai42.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class ChatController {
 
     public void sendButtonAction(ActionEvent actionEvent) {
+        // Handle send button action
     }
 
     public void sendMethod(KeyEvent keyEvent) {
+        // Handle send method
+    }
+
+    public void handleExitButtonClick(MouseEvent mouseEvent) {
+    }
+
+    public void handleSettingsButton(MouseEvent mouseEvent) {
+    }
+
+    public void startConversation(ActionEvent actionEvent) {
     }
 }
