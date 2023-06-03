8a066052d7521afa3e5462872a3522128d62aa31
Laurens <laatmijnueffe@gmail.com>
2023-06-03 12:15:08 +0200

    fix: removed unused import

--- src/main/java/nl/ai42/controllers/ChatController.java
+++ src/main/java/nl/ai42/controllers/ChatController.java
@@ -5,7 +5,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
