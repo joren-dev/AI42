9d8dce4c0419c6aae992db98d2159fd32c31c18e
Joren <contactjorenx@gmail.com>
2023-06-11 14:27:59 +0200

    change: lenght of db ctor

--- src/main/java/nl/ai42/utils/Database.java
+++ src/main/java/nl/ai42/utils/Database.java
@@ -7,45 +7,32 @@ import java.util.HashMap;
 
 public class Database implements Serializable {
     private final HashMap<String, Table> tables;
-
     private final String file_path;
 
-
-    public Database(String file_path) {
+    public Database(final String file_path) {
         this.file_path = file_path;
+        this.tables = new HashMap<>();
+        loadFromFile();
+    }
 
+    private void loadFromFile() {
         try {
             File file = new File(file_path);
             if (file.exists()) {
                 FileInputStream fileInputStream = new FileInputStream(file_path);
                 ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
                 Database result = (Database) objectInputStream.readObject();
-                this.tables = result.tables;
-            } else {
-                this.tables = new HashMap<>();
+                this.tables.putAll(result.tables);
             }
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 
-    /**
-     * Get a table by name
-     *
-     * @param name the name of the table to retrieve
-     * @return the table you requested or null if the table doesn't exist
-     */
     public Table getTable(String name) {
         return this.tables.get(name);
     }
 
-    /**
-     * Create a table with a name and a specific set of columns
-     * NOTE: if the table exists already, it is overwritten
-     *
-     * @param name the name of the table to create
-     * @param columns the names of the columns
-     */
     public void createTable(String name, String[] columns) {
         this.tables.put(name, new Table());
     }
